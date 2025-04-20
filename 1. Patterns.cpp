4920c1d935678dc0199f9791d8832422f5e4cd91
Kanav Khanna <40358658+kanav-khanna@users.noreply.github.com>
2025-04-20 20:11:40 +0530

    Add files via upload

---
+++ 1. Patterns.cpp
@@ -0,0 +1,499 @@
+/////////////////square pattern 
+#include<iostream>
+using namespace std;
+
+int main() {
+    for(int i = 0; i<4;i++) //row
+    {   
+        
+           for(int j = 0; j<4;j++) //col
+            {  
+               cout<<"*";
+           }
+        cout<<endl;|
+    }  
+    
+}
+
+/// hollow square 
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 6;
+    for(int i = 0; i<n;i++) //row
+    {   
+        
+           for(int j = 0; j<n;j++) //col
+            {  
+               if(i==0||i== n-1||j==0||j==n-1)
+               {
+                   cout<<"*";
+               }
+               else
+               {
+                   cout<<" ";
+               }
+              
+           }
+        cout<<endl; 
+    }
+
+// half pyramid hollow 
+*
+**
+* *
+*  *
+*****  
+//j == 1 → left edge
+
+//j == i → right edge
+
+//i == n → bottom row (filled with *)
+
+//Else → print space in the middle
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            // Print star at the borders and on the last row
+            if(j == 1 || j == i || i == n) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+//Inverted pyramid code 
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 8;
+    for(int i = n; i>=1;i--) //row we keep dectreasing them 
+    {       //cout<<"*"; 
+           for(int j = 1; j<=i;j++) //col fill the column with one less each time 
+            {  
+               cout<<"*";
+               
+              }
+              
+           cout<<endl;     
+    }   
+}
+
+//Hollow pyramid ///not sure how this fully works need fix
+
+#include<iostream>
+using namespace std;
+int main() {
+    int n = 8;
+    for(int i = 0; i<n;i++) //row
+    {       //cout<<"*"; 
+           for(int j = 0; j<n;j++) //col
+            {  
+               if(i == 0 || j == 0 || j == n-i-1) ///j == n-rows-1 is just a foormila that you need to remember vreated because of the corralation between the values of the triagle that need to be printed 
+               {
+                cout<<"*";
+               }
+               else{
+                   cout<<" ";
+               }
+              }
+              
+      cout<<endl;
+    }  
+}
+
+* * * * *  ////all the chars that need to be printed follow the rule cols == n-rows-1
+*     *
+*   * 
+* *
+*
+
+/// full pyramid
+If you have N number of rows then you need 2n-1 number of columns .....this is important 
+
+Why 2n-1 columns ?? 
+well if you lok at any full pyramid 
+if i have 3 rows -> 5 columns
+          4 rows -> 7 columns 
+          5 rows -> 9 columns 
+          6 rows -> 11 columns All of these follow the same formula and thats 2n-1  ////how to look at numbers and create equations ???
+
+    1 2 3 4 5 6 7 8 9        
+1            *                      
+2          * * *             
+3        * * * * *         
+4      * * * * * * *       
+5    * * * * * * * * *      
+
+so in the for loops i will need n number of rows and 2n-1 numebr of columns
+In the above figure if we look at the spots where we need to print Stars their is a pattern 
+in the first row N is printed 
+in second column N, N-1, and N+1 
+in the third one N-2, n-1, n , n+1, N+2 are printed 
+By the above logic we can use the formula N+(i-1) and N-(i+1) the i id just the column here 
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 8;
+    for(int i = 0; i<n;i++) 
+    {    
+        for(int j = 0; j<= 2*n-1; j++) //2n-1 is the formula to find the number of columns since only row is given 
+        {
+            if(j>=n-(i-1)&& j<=n+(i-1))
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+       cout<<endl; 
+    }
+
+}
+
+// full pyramid but hollow 
+i,j  1 2 3 4 5 6 7 8 9        
+1            *                         
+2          * * *             
+3        * * * * *         
+4      * * * * * * *       
+5    * * * * * * * * *  
+
+Since i need this to be hollow the following indexes need to be empty 
+i,j
+
+if (i == 1 || i == n || j == 
+Should be printed- 
+1,5 and 5 needs to be fully printed 
+2,4  2,6  n-(i-1)
+3,3  3,7
+4,2  4,8
+
+Should not be printed -  
+2,5
+3,4  3,5  3,6
+4,3  4,4  4,5  4,6  4,7
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 8;
+    for(int i = 0; i<n;i++) 
+    {    
+        for(int j = 0; j< 2*n-1; j++) 
+        {   
+            if( i == n-1){ //prints the base of the pyrimid 
+                cout<<"*";
+            }
+            else if(j ==n-i-1 || j==n+i-1) //this prints the edges//brackes removed to change output
+            {
+                
+                cout<<"*";
+                
+            }
+            else{
+                cout<<" ";
+            }
+        }
+       cout<<endl; 
+    }
+
+}
+
+//Pyramid reverse 
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 8;
+    for(int i = 0; i < n; i++) 
+    {    
+        for(int j = 0; j < 2*n - 1; j++)
+        {
+            if(j >= i && j < 2*n - 1 - i)  <- this has been updated we start with max columns then they start reducting with every step
+            {
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl; 
+    }
+}
+// reverse hollow 
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 8;
+
+    for(int i = 0; i < n; i++) 
+    {
+        for(int j = 0; j < 2*n - 1; j++)
+        {
+            if(i == 0) {
+                cout << "*"; // Top row is solid
+            }
+            else if(j == i || j == 2*n - 2 - i) { // in normal we use 2*n - 1 - i but here we use -2 since 1 row is solid   
+                cout << "*"; // Sides of the inverted pyramid
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// Diamond pattern 
+Pretty easy just combine the above 2 codes for a normal pyramid and a inverted pyramid
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 8;
+
+    // Top half (normal pyramid)
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < 2*n - 1; j++) {
+            if(j >= n - 1 - i && j <= n - 1 + i)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    // Bottom half (inverted pyramid without repeating middle line)
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < 2*n - 1; j++) {
+            if(j >= i && j < 2*n - 1 - i)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+// half pyramid pattern with numbers 
+
+1
+2*2
+3*3*3
+4*4*4*4
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+    for(int i = 1; i<=n;i++) //row we keep dectreasing them 
+    {       //cout<<"*"; 
+           for(int j = 1; j<=i;j++) //col fill the column with one less each time 
+            {  
+                cout<<i;
+               if(j < i) { //we need to add starts between all the numbers but not at the end so we check for the end 
+                cout << "*";
+            }
+           }   
+              
+              
+           cout<<endl;     
+    }   
+}
+
+// half pyramid with numbers 
+0
+12
+345
+6789
+
+int main() {
+    int n = 4;
+    int numbs = 0;
+    for(int i = 0; i<n;i++) //row we keep dectreasing them 
+    {       //cout<<"*"; 
+           for(int j = 0; j<=i;j++) //col fill the column with one less each time 
+            {  
+              cout<<numbs;
+               numbs++;
+              }
+              
+           cout<<endl;     
+    }   
+}
+
+//
+1
+12
+123
+1234
+
+int main() {
+    int n = 4;
+    char a[10] = "a";
+    for(int i = 0; i<n;i++) //row we keep dectreasing them 
+    {       //cout<<"*"; 
+           for(int j = 0; j<=i;j++) //col fill the column with one less each time 
+            {  
+              cout<<j+1;
+              }
+              
+           cout<<endl;     
+    }   
+}
+
+
+// Alphabet pyramid 
+
+A
+AB
+ABC
+ABCD
+
+
+int main() {
+    int n = 8;
+    
+    for(int i = 0; i<n;i++) 
+    {       //cout<<"*"; 
+           for(int j = 0; j<=i;j++) 
+            {  
+              char ch = j+1+'A'-1;  // this is the important part  where we convert number to char
+              cout<<ch;
+            }
+              
+           cout<<endl;     
+    }   
+}
+
+// numeric hollow half pyramid 
+12345678
+2     8 
+3    8  
+4   8   
+5  8    
+6 8     
+78  
+//GOOD :)
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 8;
+    int numb = 1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            
+            if( j == 0 ) {
+                cout << numb;
+            }
+            else if(i == 0)
+            {
+                cout<<j+1;
+                
+            }
+            else if(j == n-i-1)
+            {
+                cout<< n;
+            }
+                
+             else {
+                cout << " ";
+            }
+            
+        
+        }
+        numb++;
+        cout << endl;
+    }
+}
+
+//Floyds triangle 
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+16 17 18 19 20 21
+22 23 24 25 26 27 28
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 8;
+    int number = 1;
+    for(int i = 0; i<n;i++) 
+    {    
+        for(int j = 0; j<i; j++)
+        {   
+            cout<<" ";
+            cout<<number;
+            number++;
+        }
+       
+        cout<<endl;
+    }
+}
+
+///Numeric palindrom pyramid  *** this needs to be revised ***
+       1
+      121
+     12321
+    1234321
+   123454321
+  12345654321
+ 1234567654321
+123456787654321
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 8;
+    for(int i = 1; i <= n; i++) {
+        // Print leading spaces
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Print increasing numbers
+        for(int j = 1; j <= i; j++) { //left half
+            cout << j;
+        }
+
+        // Print decreasing numbers
+        for(int j = i - 1; j >= 1; j--) {  //right half
+            cout << j;
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
---
+++ 1.1 Bitwise incri decri.cpp
@@ -0,0 +1,116 @@
+Bitwise operator 
+
+//you will see questions from these alot in online test Bitwise ..and its usually a easy level question
+//these operations are somwhat compiler dependent 
+
+1. and &
+
+7 & 4;
+
+   1  1  1
+&  1  0 0
+   ------
+   1  0 0
+2. Or |
+    7 | 4;
+
+        1  1  1
+    |   1  0 0
+        ------
+        1   1  1
+3. NOT ~
+  
+ ~4;
+~ 1 0 0
+------
+ 0  1  1
+
+4. Xor ^
+    7 ^ 4; 0 on same value and 1 on different 
+
+       1  1  1
+    ^  1  0 0
+       ------
+        0  1  1
+
+These do bit level operations. 
+
+5. Left Shift (<<)  //n times left shit a*2^n  (2 power n) left shift is used to multiply 
+0 0 0 0 0 1 0 1 << 2
+------------
+0 0 0 1 0 1 0 0
+
+int n = 5;
+int k  = 5;
+int ans =k<<n;
+
+cout<<ans;
+
+Output 160 ........becasue 5 X (2 to the power 5 which is 32)
+
+6. Right Shift (>>)  when we shift by 1 divide my 2 ki power 1 sa will be the answer 
+//if i am doing right shift of any number by N bits the number will be divided by 2 ki power n sa 
+//But does not apply to negative numbers at times
+// if negative number and signed integer (int and not using unsigned int) then compiler will handle right shift but when i used unsigned then i get random values 
+// when we use unsigned int we get a positive large number when given a negative number as input 
+// If you try to do a right shift with a negative number you will get a garbage value this applies to both left and right shift you will get a compiler warning aswell 
+
+
+0 0 0 1 0 0 0 0 >> 2
+------------
+0 0 0 0 0 1 0 0
+
+
+// VV important concept for MCQ////////////////////////////////
+Pre/post increment decrement operator 
+a++ a--
+
+Pre increment - Phele istemal kere fir wishwas kara (first incriment then use)  int n = 5 cout<< (++n); Output = 6
+Post increment - Phele use karo then increment karo (first use value then increment value) int n = 5 cout<< (n++); Output = 5
+Pre decrement - Phele decrement then use
+Post decrement - Use the value then decrement 
+int a = 10
+Cout<<(++a)*(a++) output = 121/132
+Cout<<((a++)*(++a)) output = 120  //Need to look into why this is happening 
+
+// Break and continue /////////////////////////////////
+
+Break- 
+as soon as break statement is reached in a loop you will come out of the loop 
+
+Continue- 
+helps skip iterations in a loop 
+for(int i = 0; i<10; i++)
+   {
+      if(i = 2)
+         {
+            continue  //the second iteration will be skipped and the lower code will be run on the next iteration 
+         }
+      //rest of the code of the loop
+   }
+
+//////// Variable scoping ///////////////// 
+
+1. local variable 
+   
+for(int i = 0; i<10; i++)
+   {
+      cout<<i; //this will give the correct value of i;
+   }
+   cout<<i; // this will not work since i is a local variable 
+
+
+2. globle variable 
+created outside any loops or funtions and accessable everywhere
+
+/////Expression//////////////
+
+2*3+5/10-2  ///how i would solve this using BODMAS but the computer does not have that 
+6+0.5-2
+4.5
+// the compiler uses a operator precidence table which is uses to accurately solve equations 
+// We shoud use brackets for more accurate equations 
+//The above equation should be written as this so that thier are no issues with inaccurate answers
+
+
+((2*3)+(5/10))-2
