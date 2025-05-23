784e82da5f2e75e4d8d912cfc9dc3e79c02cae59
Kanav Khanna <40358658+kanav-khanna@users.noreply.github.com>
2025-05-23 20:47:47 +0530

    asd

---
+++ 6.1 Pointers.cpp
@@ -0,0 +1,30 @@
+
+What are pointers ?
+
+A type of variable that stores the address of of other variables is a pointer.
+
+How do we see the address of a variable  - & (address of operator)
+the meomery address in printed in a hexadecimal format
+
+//Pointer creation 
+Int* ptr = address
+
+
+//Access value stored at the address in the pointer 
+Use de refrence operator 
+cout<< *ptr //this should print the value if what the pointer points to 
+
+
+//*ptr = *ptr +1
+*ptr is the value present at the address stored in ptr 
+so in the code above i am adding + 1 to the location storued at the value  
+
+Arr
+&arr   ---all 3 of these will print the same thing which is the base address of the array 
+&arr[0] 
+
+
+*(i+arr) is the same as i[arr]
+
+
+//Refer to the codes its just better like that lol
