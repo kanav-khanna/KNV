3d691d675b5a0028185987825272c6166025bd85
Kanav Khanna <40358658+kanav-khanna@users.noreply.github.com>
2025-08-30 14:01:34 +0530

    backup

---
+++ 10 linked list.cpp
@@ -0,0 +1,21 @@
+//Read about operating system storage management linked list use
+
+//I always miss some testcases in OA ....some edge cases need to be better handled
+
+
+Types-
+1.singly
+2.doubly
+3.circular singly
+4.circular doubly
+
+
+
+
+
+
+
+
+
+
+
